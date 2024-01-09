078d6ac1faf89073925cc2a56552b73ace5b0f62
Laminito <mba25116@gmail.com>
2024-01-09 13:34:57 +0000

    update product ok

--- src/app/product/form/form.component.ts
+++ src/app/product/form/form.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
-import {ProductService} from "../../service/product.service";
-import {ActivatedRoute} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {CategoriService} from "../../service/categori.service";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {CONSTANTES} from "../../constantes/constantes";
+import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CategoriService } from '../../service/categori.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CONSTANTES } from '../../constantes/constantes';
 import swal from 'sweetalert2';
 
 @Component({
@@ -12,98 +12,106 @@ import swal from 'sweetalert2';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit{
-  idProduct:any;
-  productsTypes! :any;
+export class FormComponent implements OnInit {
+  idProduct: any;
+  productsTypes!: any;
+  type!: any;
   dialogTitle!: string;
-  action!:string;
+  action!: string;
   constantes = CONSTANTES.TYPEACTION;
-  productForm:FormGroup = new FormGroup<any>({
-      productName:new FormControl(''),
-      productType:new FormControl('')
-    })
-  ngOnInit() {
-    this.getProductTypes();
-  }
+  productForm: FormGroup = new FormGroup<any>({
+    productName: new FormControl(''),
+    productType: new FormControl('')
+  });
 
-  onSave(){
-    if (this.action == this.constantes.NEW){
-      this.save();
-    }else if(this.action == this.constantes.EDIT){
-      this.edit();
-    }
-
-  }
-
-  getProduct(idProduct:string){
-    if (idProduct){
-      this.productService.product(idProduct).subscribe(res=>{
-        const product =  res.modelsis.result.data
-        console.log('resultat',res);
-        this.productForm.patchValue({
-          productName: product.name,
-          productType: product.type
-        })
-      })
-    }
-  }
-  getProductTypes(){
-    this.productTypeService.listTypeProduct().subscribe(res=>{
-      this.productsTypes = res.modelsis.result.data;
-      console.log('type de produits',this.productsTypes)
-    })
-  }
-  constructor(private productService:ProductService,
-              private router:ActivatedRoute,
-              private productTypeService:CategoriService,
-              public matDialogRef: MatDialogRef<FormComponent>,
-              private changeDetectorRef: ChangeDetectorRef,
-              @Inject(MAT_DIALOG_DATA) _data: any) {
-    console.log('data',_data)
-    if (_data.action == this.constantes.NEW){
-      this.dialogTitle = "Ajouter un ";
-      this.action = this.constantes.NEW
-    }else if(_data.action == this.constantes.EDIT){
-      this.dialogTitle = "Modifier un ";
-      this.idProduct  = _data.information.idProduct
-      this.getProduct(this.idProduct)
+  constructor(
+    private productService: ProductService,
+    private router: ActivatedRoute,
+    private productTypeService: CategoriService,
+    public matDialogRef: MatDialogRef<FormComponent>,
+    private changeDetectorRef: ChangeDetectorRef,
+    @Inject(MAT_DIALOG_DATA) private _data: any
+  ) {
+    if (_data.action === this.constantes.NEW) {
+      this.dialogTitle = 'Ajouter un ';
+      this.action = this.constantes.NEW;
+    } else if (_data.action === this.constantes.EDIT) {
+      this.dialogTitle = 'Modifier un ';
+      this.idProduct = _data.information.idProduct;
+      this.type = _data.information.type;
+      this.getProduct(this.idProduct);
       this.action = this.constantes.EDIT;
     }
   }
 
-  private save() {
-    this.productForm.patchValue({productType:{id:this.productForm.get('productType')?.value}})
-    if (this.productForm.valid){
-      this.productService.addProduct(this.productForm.value).subscribe(res=>{
-        this.matDialogRef.close(res);
-        this.changeDetectorRef.markForCheck();
-        console.log('ajout', res)
-        swal.fire({
-          title: res.modelsis.result.status,
-          text: res.modelsis.result.message,
-          icon: "success"
-        });
-      })
-    }
-
+  ngOnInit() {
+    this.getProductTypes();
   }
-/*  private edit() {
-    this.productForm.patchValue({id:this.idProduct,productType:{id:this.productForm.get('productType')?.value}})
-    console.log('edit : ',this.productForm.get('productType')?.value)
-    if (this.productForm.valid){
-      this.productService.editProduct(this.productForm.value).subscribe(res=>{
+
+  onSave() {
+    if (this.action === this.constantes.NEW) {
+      this.save();
+    } else if (this.action === this.constantes.EDIT) {
+      this.edit();
+    }
+  }
+
+  getProduct(idProduct: string) {
+    if (this.idProduct) {
+      this.productService.product(this.idProduct).subscribe(res => {
+        const product = res.modelsis.result.data;
+        this.productForm.patchValue({
+          productName: product.name,
+          productType: product.type
+        });
+
+        if (this.productsTypes && this.productsTypes.length > 0) {
+          const selectedProductType = this.productsTypes.find((type: any) => type.id === product.type.id);
+          if (selectedProductType) {
+            this.productForm.get('productType')?.setValue(selectedProductType.type);
+          }
+        }
+      });
+    }
+  }
+
+  getProductTypes() {
+    this.productTypeService.listTypeProduct().subscribe(res => {
+      this.productsTypes = res.modelsis.result.data;
+      console.log('type de produits', this.productsTypes);
+    });
+  }
+
+  private save() {
+    this.productForm.patchValue({ productType: { id: this.productForm.get('productType')?.value } });
+    if (this.productForm.valid) {
+      this.productService.addProduct(this.productForm.value).subscribe(res => {
         this.matDialogRef.close(res);
         this.changeDetectorRef.markForCheck();
-        console.log('ajout', res)
+        console.log('ajout', res);
+        if (res.modelsis.result.status === 'Success') {
+          swal.fire({
+            title: res.modelsis.result.status,
+            text: res.modelsis.result.message,
+            icon: 'success'
+          });
+        } else {
+          swal.fire({
+            title: res.modelsis.result.status,
+            text: res.modelsis.result.message,
+            icon: 'error'
+          });
+        }
+      }, (error) => {
         swal.fire({
-          title: res.modelsis.result.status,
-          text: res.modelsis.result.message,
-          icon: "success"
+          title: 'Failed',
+          text: 'Failed to add the product',
+          icon: 'error'
         });
-      })
+      });
     }
+  }
 
-  }*/
 
   private edit() {
     if (this.productForm.valid) {
@@ -119,15 +127,27 @@ export class FormComponent implements OnInit{
         this.changeDetectorRef.markForCheck();
         console.log('Edition du produit :', res);
 
+        if (res.modelsis.result.status === 'success') {
+          swal.fire({
+            title: res.modelsis.result.status,
+            text: res.modelsis.result.message,
+            icon: 'success'
+          });
+        } else {
+          swal.fire({
+            title: res.modelsis.result.status,
+            text: res.modelsis.result.message,
+            icon: 'error'
+          });
+        }
+      }, (error) => {
         swal.fire({
-          title: res.modelsis.result.status,
-          text: res.modelsis.result.message,
-          icon: "success"
+          title: 'Failed',
+          text: 'Failed to edit the product',
+          icon: 'error'
         });
       });
     }
   }
-
-
 
 }
--- src/app/product/product/form-edit/form-edit.component.ts
+++ src/app/product/product/form-edit/form-edit.component.ts
@@ -1,30 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {ProductService} from "../../../service/product.service";
-import {ActivatedRoute} from "@angular/router";
-import {CategoriService} from "../../../service/categori.service";
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProductService } from '../../../service/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { CategoriService } from '../../../service/categori.service';
 
 @Component({
   selector: 'app-form-edit',
   templateUrl: './form-edit.component.html',
-  styleUrls: ['./form-edit.component.css']
+  styleUrls: ['./form-edit.component.css'],
 })
-export class FormEditComponent implements OnInit{
-  objectProduct = {
-    id:'',
-    productName: '',
-    productType: {
-      id: '',
-    },
-  }
-
+export class FormEditComponent implements OnInit {
   idProduct = this.router.snapshot.params['id'];
-  productsTypes! :any;
-  productForm:FormGroup = new FormGroup<any>({
-    productName:new FormControl(''),
-    productType:new FormControl('')
-  })
-
+  productsTypes: any;
+  typeProduct : any;
+  productForm: FormGroup = new FormGroup<any>({
+    productName: new FormControl(''),
+    productType: new FormControl(''),
+  });
 
   ngOnInit() {
     this.getProductTypes();
@@ -41,13 +34,23 @@ export class FormEditComponent implements OnInit{
     if (this.idProduct) {
       this.productService.product(this.idProduct).subscribe((res) => {
         const product = res.modelsis.result.data;
+
         this.productForm.patchValue({
           productName: product.productName,
-          productType: product.productType.id // Utilisation de l'identifiant du type de produit
+          productType: product.productType.id,
         });
+
+        const selectedProductType = this.productsTypes.find((type: any) => type.type === product.productType);
+        console.log('selectedProductType'+selectedProductType)
+        if (selectedProductType) {
+          this.productForm.get('productType')?.setValue(selectedProductType.id);
+          console.log("this.productForm.get('productType')?.setValue(selectedProductType.id)'+selectedProductType"+this.productForm.get('productType')?.setValue(selectedProductType.id))
+        }
       });
     }
   }
+
+
 
   private edit(id: string) {
     if (this.productForm.valid) {
@@ -55,25 +58,29 @@ export class FormEditComponent implements OnInit{
         id: id,
         productName: this.productForm.get('productName')?.value,
         productType: {
-          id: this.productForm.get('productType')?.value
-        }
+          id: this.productForm.get('productType')?.value,
+        },
       };
 
+
+
       this.productService.editProduct(editedProduct).subscribe((res) => {
-        console.log('editProduct',res);
+        console.log('editProduct', res);
       });
     }
   }
 
-  getProductTypes(){
-    this.productTypeService.listTypeProduct().subscribe(res=>{
-      this.productsTypes = res.modelsis.result.data;
-      console.log(this.productsTypes)
-    })
-  }
-  constructor(private productService:ProductService,private router:ActivatedRoute, private productTypeService:CategoriService) {
-  }
+    getProductTypes() {
+      this.productTypeService.listTypeProduct().subscribe((res) => {
+        this.productsTypes = res.modelsis.result.data;
+        console.log(this.productsTypes);
+      });
+    }
 
-
-
+  constructor(
+    private productService: ProductService,
+    private router: ActivatedRoute,
+    private productTypeService: CategoriService
+  ) {}
 }
+
